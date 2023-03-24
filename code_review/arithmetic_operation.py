b57b12b662bc745569df12e95bd6dd14f6fc782d
JohnZDictator <yohannesketemazeleke@gmail.com>
2023-03-24 13:07:09 +0300

    fix(testing and code review): code review and testing practice examples

---
+++ code_review/arithmetic_operation.py
@@ -0,0 +1,13 @@
+class ArithmeticOperations:
+    def operation_1(x, y):
+        return x + y
+    def operation_2(x, y):
+        return x - y
+    def operation_3(x, y):
+        return x * y
+    def operation_4(x, y):
+        return x / y
+    def speak(x):
+        return x
+    def sleep(x):
+        return f"I am gonna slee {x} hours"
