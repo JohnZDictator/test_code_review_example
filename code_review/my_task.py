abbeb1e03e74ecf178e60ba409040278299cf9b2
JohnZDictator <yohannesketemazeleke@gmail.com>
2023-03-24 14:18:19 +0300

    fix(arithmetic operation): arithmetic operation

--- code_review/my_task.py
+++ code_review/my_task.py
@@ -7,7 +7,3 @@ class Myclass:
         return x * y
     def operation_4(x, y):
         return x / y
-    def speak(x):
-        return x
-    def sleep(x):
-        return f"I am gonna slee {x} hours"
